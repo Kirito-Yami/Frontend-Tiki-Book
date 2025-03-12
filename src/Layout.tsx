912efea5742b2a44ef5f008d963718412d3a13ec
Kirito-Yami <minhducnguyen.developer@gmail.com>
2025-03-12 19:17:29 +0700

    Fix Loading

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,25 +3,43 @@ import {Outlet} from "react-router-dom";
 import {useCurrentApp} from "components/context/app.context.tsx";
 import {useEffect} from "react";
 import {fetchAccountAPI} from "services/api.ts";
+import {PacmanLoader} from "react-spinners";
 
 const Layout = () => {
-    const { setUser, isAppLoading, setIsAppLoading } = useCurrentApp();
+    const {setUser, isAppLoading, setIsAppLoading, setIsAuthenticated} = useCurrentApp();
     useEffect(() => {
         const fetchAccount = async () => {
             const res = await fetchAccountAPI();
             if (res.data) {
-                setUser(res.data.user)
+                setUser(res.data.user);
+                setIsAuthenticated(true);
             }
             setIsAppLoading(false)
         }
         fetchAccount();
     }, [])
     return (
-        <div>
-            <AppHeader />
-            <Outlet />
-        </div>
+        <>
+            {!isAppLoading ?
+                <div>
+                    <AppHeader/>
+                    <Outlet/>
+                </div>
+                :
+                <div style={{
+                    position: "fixed",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)"
+                }}>
+                    <PacmanLoader
+                        size={30}
+                        color="#36d6b4"
+                    />
+                </div>
+            }
+        </>
     )
 }
 
-export default Layout
+export default Layout;
