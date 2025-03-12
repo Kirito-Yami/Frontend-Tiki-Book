0f1994506b38cf1e17a1bef0241cbde195b260a5
Kirito-Yami <minhducnguyen.developer@gmail.com>
2025-03-12 16:42:57 +0700

    Design Page Register

--- src/main.tsx
+++ src/main.tsx
@@ -1,50 +1,45 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import Layout from './Layout.tsx';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
 import BookPage from "pages/client/book.tsx";
 import AboutPage from "pages/client/about.tsx";
 import LoginPage from "pages/client/auth/login.tsx";
 import RegisterPage from "pages/client/auth/register.tsx";
 import HomePage from "pages/client/home.tsx";
 import 'styles/global.scss';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Layout/>,
+        element: <Layout />,
         children: [
             {
                 index: true,
-                element: <HomePage/>
+                element: <HomePage />,
             },
             {
-                path: "/book",
-                element: <BookPage/>,
+                path: "book",
+                element: <BookPage />,
             },
             {
-                path: "/about",
-                element: <AboutPage/>,
+                path: "about",
+                element: <AboutPage />,
             },
-
-        ]
+        ],
     },
     {
         path: "/login",
-        element: <LoginPage/>,
+        element: <LoginPage />,
     },
     {
         path: "/register",
-        element: <RegisterPage/>,
+        element: <RegisterPage />,
     },
-
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <RouterProvider router={router}/>
-    </StrictMode>,
-)
+        <RouterProvider router={router} />
+    </StrictMode>
+);
