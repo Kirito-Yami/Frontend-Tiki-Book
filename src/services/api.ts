4c3f152276032708290c3a5838513ad993ae2444
Kirito-Yami <minhducnguyen.developer@gmail.com>
2025-03-12 17:53:41 +0700

    Axios Customize (Part 2)

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'services/axios.customize';
 
 const loginAPI = (username: string, password: string) => {
     const urlBackend = "/api/v1/auth/login";
-    return axios.post(urlBackend, {username, password})
+    return axios.post<IBackendRes<ILogin>>(urlBackend, { username, password })
 }
 
 export {loginAPI}
