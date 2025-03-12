ee34dab1ed72d1abf65dc5b18a3dd458420306c4
Kirito-Yami <minhducnguyen.developer@gmail.com>
2025-03-12 15:28:18 +0700

    Dividing Layout and Project Structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dns from 'dns'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 dns.setDefaultResultOrder('verbatim')
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  }
 })
